d8d46d621728ff922d25540af59bdedfc88724ad
Grant Goodale <grant@girder.ai>
2025-03-09 20:27:55 -0700

    Use .zip extension when downloading Gleam on Windows (#10)
    Windows distributions of the gleam executable use a zipfile rather than
    a tarball; this PR enables the extension to download the binary
    successfully.
    
    Since Windows support in Zed is a bit raw right now, I don't *think*
    this does anything unexpected but it's very difficult to test. Feedback
    welcome.
    
    ---------
    
    Co-authored-by: Marshall Bowers <git@maxdeviant.com>

--- src/gleam.rs
+++ src/gleam.rs
@@ -43,7 +43,7 @@ impl GleamExtension {
 
         let (platform, arch) = zed::current_platform();
         let asset_name = format!(
-            "gleam-{version}-{arch}-{os}.tar.gz",
+            "gleam-{version}-{arch}-{os}.{ext}",
             version = release.version,
             arch = match arch {
                 zed::Architecture::Aarch64 => "aarch64",
@@ -54,6 +54,10 @@ impl GleamExtension {
                 zed::Os::Mac => "apple-darwin",
                 zed::Os::Linux => "unknown-linux-musl",
                 zed::Os::Windows => "pc-windows-msvc",
+            },
+            ext = match platform {
+                zed::Os::Mac | zed::Os::Linux => "tar.gz",
+                zed::Os::Windows => "zip",
             },
         );
 
@@ -75,7 +79,10 @@ impl GleamExtension {
             zed::download_file(
                 &asset.download_url,
                 &version_dir,
-                zed::DownloadedFileType::GzipTar,
+                match platform {
+                    zed::Os::Mac | zed::Os::Linux => zed::DownloadedFileType::GzipTar,
+                    zed::Os::Windows => zed::DownloadedFileType::Zip,
+                },
             )
             .map_err(|e| format!("failed to download file: {e}"))?;
 
